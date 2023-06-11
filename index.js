8ead6be0dc05e891c86d9efb97d2ec2d7272db42
Muhamad Syamim Irfan Bin Ahmad Shokkri <syamimirfan59@gmail.com>
2023-06-12 02:17:57 +0800

    Backend deployment

--- index.js
+++ index.js
@@ -11,35 +11,35 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //get Admin controller
-const adminRouter = require('../WEMEET-DAPPS-BACKEND/controller/Admin');
+const adminRouter = require('./controller/Admin');
 app.use('/admin', adminRouter);
 
 // get Student controller
-const studentRouter = require('../WEMEET-DAPPS-BACKEND/controller/Student');
+const studentRouter = require('./controller/Student');
 app.use('/student', studentRouter);
 
 //get Lecturer controller
-const lecturerRouter = require('../WEMEET-DAPPS-BACKEND/controller/Lecturer');
+const lecturerRouter = require('./controller/Lecturer');
 app.use('/lecturer', lecturerRouter);
 
 //get Slot controller
-const slotRouter = require('../WEMEET-DAPPS-BACKEND/controller/Slot');
+const slotRouter = require('./controller/Slot');
 app.use('/slot', slotRouter);
 
 //get Booking controller
-const bookingRouter = require('../WEMEET-DAPPS-BACKEND/controller/Booking');
+const bookingRouter = require('./controller/Booking');
 app.use('/booking', bookingRouter);
 
 //get Attendance controller
-const attendanceRouter = require('../WEMEET-DAPPS-BACKEND/controller/Attendance');
+const attendanceRouter = require('./controller/Attendance');
 app.use('/attendance', attendanceRouter);
 
 //get Hash controller
-const hashController = require('../WEMEET-DAPPS-BACKEND/controller/Hash');
+const hashController = require('./controller/Hash');
 app.use('/hash', hashController);
 
 //get Chat controller
-const chatController = require('../WEMEET-DAPPS-BACKEND/controller/Chat');
+const chatController = require('./controller/Chat');
 app.use('/chat', chatController);
 
 
